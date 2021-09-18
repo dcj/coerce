17e09d428d827f70ae00eacc075ec7c8fed8a906
Donald Clark Jackson <dcj@clark-communications.com>
2021-09-18 14:46:51 -0700

    remove cheshire

--- src/coerce/jdbc/pg.clj
+++ src/coerce/jdbc/pg.clj
@@ -7,7 +7,6 @@
    [next.jdbc.result-set :as result-set]
    [next.jdbc.prepare    :as prepare]
    [geo.io]
-   ;; [cheshire.core :as json]
    [clojure.data.json :as json]
    )
   (:import [org.postgresql.util PGobject]
@@ -21,8 +20,6 @@
            [org.threeten.extra Interval]
            )
   )
-
-;; TODO: Remove cheshire
 
 
 ;;
@@ -41,7 +38,6 @@
   [data json-type]
   (doto (PGobject.)
     (.setType (name json-type))
-    ;; (.setValue (json/generate-string data))))
     (.setValue (json/write-str data))))
 
 ;;
@@ -204,13 +200,11 @@
 (defmethod pgobject->clj :json
   [^org.postgresql.util.PGobject x]
   (when-let [val (.getValue x)]
-    ;; (json/parse-string val true)))
     (json/read-str val :key-fn keyword)))
 
 (defmethod pgobject->clj :jsonb
   [^org.postgresql.util.PGobject x]
   (when-let [val (.getValue x)]
-    ;; (json/parse-string val true)))
     (json/read-str val :key-fn keyword)))
 
 ;; PostgreSQL comes with the following built-in range types:
